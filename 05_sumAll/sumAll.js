1008de767ae8d8e1a417ef0d5b809856563d160f
juansemolina777 <106198574+juansemolina777@users.noreply.github.com>
2025-02-25 20:01:30 -0300

    Update sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,16 @@
 const sumAll = function() {
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  if (min < 0 || max < 0) return "ERROR";
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+   let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return: sum
 
 };
 
